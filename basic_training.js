2ba3f3b8292381561ab2cfb4c69a2d0c5ef99fa4
schroeer <schroeer@9472904b-5c12-0410-92ad-c346ed384f73>
2018-01-01 22:44:15 +0000

    nach index.html verschoben

--- basic_training.js
+++ basic_training.js
@@ -1,136 +1,238 @@
-var test_set = {
-    "title": "Hang on straight arms",
-    "left":  1,
-    "right": 1,
-    "hold":  7,
-    "break": 3,
-    "reps":  3,
-};
-
-var training1 = {
-    "title":        "Example training",
-    "description":  "A simple example training with two sets of exercises",
-    "board":        "bm1000",
-    "sets":         [
-        {
-            "title":        "Warm up",
-            "description":  "Hang on straight arms",
-            "left":         1,
-            "right":        21,
-            "hold":         3,
-            "break":        2,
-            "reps":         3,
-            "pause":        20,
-        },
-        {
-            "title":        "Pull up 2x and stay",
-            "description":  "Pull up twice and stay in locked position",
-            "left":         5,
-            "right":        25,
-            "hold":         7,
-            "break":        3,
-            "reps":         3,
-            "pause":        20,
-        }
-    ]
-};
-
-var board1 = {
-    "id":    "bm1000",
-    "name":  "Beastmaker 1000",
-    "image": "beastmaker1000.png",
-    "holds": {
-        "1":    {
-            "name": "jug",
-            "image": "bm1000_01.png"
-        },
-        "2":    {
-            "name": "hard sloper",
-            "image": "bm1000_02.png"
-        },
-        "3":    {
-            "name": "easy sloper",
-            "image": "bm1000_03.png"
-        },
-        "4":    {
-            "name": "hard four finger pocket",
-            "image": "bm1000_04.png"
-        },
-        "5":    {
-            "name": "easy four finger pocket",
-            "image": "bm1000_05.png"
-        },
-        "6":    {
-            "name": "medium four finger pocket",
-            "image": "bm1000_06.png"
-        },
-        "7":    {
-            "name": "easy two finger pocket",
-            "image": "bm1000_07.png"
-        },
-        "8":    {
-            "name": "hard two finger pocket",
-            "image": "bm1000_08.png"
-        },
-        "9":    {
-            "name": "easy three finger pocket",
-            "image": "bm1000_09.png"
-        },
-        "10":   {
-            "name": "hard three finger pocket",
-            "image": "bm1000_10.png"
-        },
-        "11":   {
-            "name": "medium three finger pocket",
-            "image": "bm1000_11.png"
-        },
-        "21":   {
-            "name": "jug",
-            "image": "bm1000_21.png"
-        },
-        "22":   {
-            "name": "hard sloper",
-            "image": "bm1000_22.png"
-        },
-        "23":   {
-            "name": "easy sloper",
-            "image": "bm1000_23.png"
-        },
-        "24":   {
-            "name": "hard four finger pocket",
-            "image": "bm1000_24.png"
-        },
-        "25":   {
-            "name": "easy four finger pocket",
-            "image": "bm1000_25.png"
-        },
-        "26":   {
-            "name": "medium four finger pocket",
-            "image": "bm1000_26.png"
-        },
-        "27":   {
-            "name": "easy two finger pocket",
-            "image": "bm1000_27.png"
-        },
-        "28":   {
-            "name": "hard two finger pocket",
-            "image": "bm1000_28.png"
-        },
-        "29":   {
-            "name": "easy three finger pocket",
-            "image": "bm1000_29.png"
-        },
-        "30":   {
-            "name": "hard three finger pocket",
-            "image": "bm1000_30.png"
-        },
-        "31":   {
-            "name": "medium three finger pocket",
-            "image": "bm1000_31.png"
-        },
-        "40":   {
-            "name": "central pocket",
-            "image": "bm1000_40.png"
+var boards = [
+    {
+        "id":    "bm1000",
+        "name":  "Beastmaker 1000",
+        "image": "bm1000.png",
+        "holds": {
+            "1":    {
+                "name": "jug",
+                "image": "bm1000_01.png"
+            },
+            "2":    {
+                "name": "hard sloper",
+                "image": "bm1000_02.png"
+            },
+            "3":    {
+                "name": "easy sloper",
+                "image": "bm1000_03.png"
+            },
+            "4":    {
+                "name": "hard four finger pocket",
+                "image": "bm1000_04.png"
+            },
+            "5":    {
+                "name": "easy four finger pocket",
+                "image": "bm1000_05.png"
+            },
+            "6":    {
+                "name": "medium four finger pocket",
+                "image": "bm1000_06.png"
+            },
+            "7":    {
+                "name": "easy two finger pocket",
+                "image": "bm1000_07.png"
+            },
+            "8":    {
+                "name": "hard two finger pocket",
+                "image": "bm1000_08.png"
+            },
+            "9":    {
+                "name": "easy three finger pocket",
+                "image": "bm1000_09.png"
+            },
+            "10":   {
+                "name": "hard three finger pocket",
+                "image": "bm1000_10.png"
+            },
+            "11":   {
+                "name": "medium three finger pocket",
+                "image": "bm1000_11.png"
+            },
+            "21":   {
+                "name": "jug",
+                "image": "bm1000_21.png"
+            },
+            "22":   {
+                "name": "hard sloper",
+                "image": "bm1000_22.png"
+            },
+            "23":   {
+                "name": "easy sloper",
+                "image": "bm1000_23.png"
+            },
+            "24":   {
+                "name": "hard four finger pocket",
+                "image": "bm1000_24.png"
+            },
+            "25":   {
+                "name": "easy four finger pocket",
+                "image": "bm1000_25.png"
+            },
+            "26":   {
+                "name": "medium four finger pocket",
+                "image": "bm1000_26.png"
+            },
+            "27":   {
+                "name": "easy two finger pocket",
+                "image": "bm1000_27.png"
+            },
+            "28":   {
+                "name": "hard two finger pocket",
+                "image": "bm1000_28.png"
+            },
+            "29":   {
+                "name": "easy three finger pocket",
+                "image": "bm1000_29.png"
+            },
+            "30":   {
+                "name": "hard three finger pocket",
+                "image": "bm1000_30.png"
+            },
+            "31":   {
+                "name": "medium three finger pocket",
+                "image": "bm1000_31.png"
+            },
+            "40":   {
+                "name": "central pocket",
+                "image": "bm1000_40.png"
+            }
         }
     }
-};
+];
+
+var trainings = [
+    {
+        "title":        "Little Boy",
+        "description":  "Lots of hanging on good holds",
+        "board":        "bm1000",
+        "sets":         [
+            {
+                "title":        "Warm up",
+                "description":  "Hang on straight arms",
+                "left":         1,
+                "right":        21,
+                "hold":         7,
+                "break":        4,
+                "reps":         7,
+                "pause":        15,
+            },
+            {
+                "title":        "Pull up 2x and hang",
+                "description":  "Pull up twice, then hang on straight arms",
+                "left":         1,
+                "right":        21,
+                "hold":         10,
+                "break":        10,
+                "reps":         3,
+                "pause":        120,
+            },
+            {
+                "title":        "4 fingers hang",
+                "description":  "Hang on straight arms",
+                "left":         5,
+                "right":        25,
+                "hold":         6,
+                "break":        4,
+                "reps":         4,
+                "pause":        90,
+            },
+            {
+                "title":        "3 fingers right side",
+                "description":  "Pull up once and stay in locked position",
+                "left":         1,
+                "right":        29,
+                "hold":         4,
+                "break":        7,
+                "reps":         4,
+                "pause":        120,
+            },
+            {
+                "title":        "3 fingers left side",
+                "description":  "Pull up once and stay in locked position",
+                "left":         9,
+                "right":        21,
+                "hold":         4,
+                "break":        7,
+                "reps":         4,
+                "pause":        120,
+            },
+            {
+                "title":        "4 fingers pull up",
+                "description":  "Pull up once, then hang on straight arms",
+                "left":         5,
+                "right":        25,
+                "hold":         5,
+                "break":        4,
+                "reps":         5,
+                "pause":        120,
+            },
+            {
+                "title":        "4 fingers hang",
+                "description":  "Hang on straight arms",
+                "left":         5,
+                "right":        25,
+                "hold":         7,
+                "break":        4,
+                "reps":         7,
+                "pause":        120,
+            },
+            {
+                "title":        "Jug hang",
+                "description":  "Hang on straight arms",
+                "left":         1,
+                "right":        21,
+                "hold":         7,
+                "break":        4,
+                "reps":         7,
+                "pause":        120,
+            }
+        ]
+    },
+    {
+        "title":        "Ralfi Beginner", // https://ralfisordinarylife.files.wordpress.com/2016/02/hangboard-training-plan-beginner.pdf
+        "description":  "Hangboard training plan for beginners",
+        "board":        "bm1000",
+        "sets":         [
+            {
+                "title":        "4 fingers jug",
+                "description":  "Hang",
+                "left":         1,
+                "right":        21,
+                "hold":         6,
+                "break":        4,
+                "reps":         4,
+                "pause":        16,
+            },
+            {
+                "title":        "open 3 fingers",
+                "description":  "Hang",
+                "left":         9,
+                "right":        29,
+                "hold":         6,
+                "break":        4,
+                "reps":         4,
+                "pause":        140,
+            },
+            {
+                "title":        "4 fingers half-crimp",
+                "description":  "Hang",
+                "left":         6,
+                "right":        26,
+                "hold":         6,
+                "break":        4,
+                "reps":         4,
+                "pause":        140,
+            },
+            {
+                "title":        "4 fingers sloper",
+                "description":  "Hang",
+                "left":         3,
+                "right":        23,
+                "hold":         6,
+                "break":        4,
+                "reps":         4,
+                "pause":        140,
+            }
+        ]
+    }
+];
--- trainer.js
+++ trainer.js
@@ -1,4 +1,6 @@
-// pscp -l schroeer -i ..\Gipfelkreuz\private_key.ppk -r basic_training.js images index.html test1.js trainer.html trainer.js daimlerstr.de:stella/www/trainer/
+// pscp -l schroeer -i ..\Gipfelkreuz\private_key.ppk -r basic_training.js sound.js images index.html trainer.js daimlerstr.de:stella/www/trainer/
+
+"use strict";
 
 var counter_div = document.getElementById("counter");
 var hold_pbar = document.getElementById("hold_pbar");
@@ -7,12 +9,7 @@ var break_pbar = document.getElementById("break_pbar");
 var set_title_div = document.getElementById("set_title");
 var set_description_div = document.getElementById("set_description");
 
-var overlay_left_img = document.getElementById("overlay_left");
-var overlay_right_img = document.getElementById("overlay_right");
-
 var pause_button = document.getElementsByName("pause")[0];
-
-var board = board1; // muss gesucht werden
 
 var synth = window.speechSynthesis;
 var voice;
@@ -29,21 +26,31 @@ function selectVoice() {
         console.log('selected voice: ' + voice.name);
     }
 }
-
-function Sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
+function ticSound() {
+    soundEffect(
+        523.25,       //frequency
+        0.05,         //attack
+        0.2,          //decay
+        "sine",       //waveform
+        10,            //volume
+        0.8,          //pan
+        0,            //wait before playing
+        600,          //pitch bend amount
+        true,         //reverse
+        100,          //random pitch range
+        0,            //dissonance
+        undefined,    //echo: [delay, feedback, filter]
+        undefined     //reverb: [duration, decay, reverse?]
+    );
 }
 
-Sound.prototype.play = function(){
-    this.sound.play();
-}
-Sound.prototype.stop = function(){
-    this.sound.pause();
+function completedSound() {
+    //D
+    soundEffect(587.33, 0, 0.2, "square", 1, 0, 0);
+    //A
+    soundEffect(880, 0, 0.2, "square", 1, 0, 0.1);
+    //High D
+    soundEffect(1174.66, 0, 0.3, "square", 1, 0, 0.2);
 }
 
 var counter = (function () {
@@ -90,8 +97,6 @@ var counter = (function () {
     }
 })();
 
-pause_button.addEventListener("click", counter.pause);
-
 async function runSet(set) {
     
     var utter_go = new SpeechSynthesisUtterance("Go!");
@@ -122,7 +127,7 @@ async function runSet(set) {
                 hold_pbar.value = step + 1;
             }
         );
-        finish_sound.play();
+        await completedSound();
 
         console.log(`rep ${rep+1}: break`);
         await counter.start(
@@ -138,7 +143,9 @@ async function runSet(set) {
     console.log("done");
 }
 
-async function runTraining(training) {
+async function runTraining(board_id, training) {
+    var board = getBoard(board_id);
+
     for (var i in training.sets) {
         var set = training.sets[i];
         if (set.pause < 15) {
@@ -159,8 +166,9 @@ async function runTraining(training) {
         set_title_div.textContent = "Pause";
         set_description_div.textContent = "Pause for " + set.pause + " seconds";
         
-        overlay_left_img.src = "";
-        overlay_right_img.src = "";
+        document.querySelectorAll("#run_content .overlay_img").forEach(function(element) {
+            element.src = "";
+        });
 
         pause_pbar.max = set.pause;
         pause_pbar.style.display = "inline-block";
@@ -171,7 +179,7 @@ async function runTraining(training) {
         await counter.start(
             set.pause,
             1000,
-            function(step) {
+            async function(step) {
                 counter_div.textContent = set.pause - step;
                 pause_pbar.value = step + 1;
                 
@@ -182,11 +190,11 @@ async function runTraining(training) {
                     set_title_div.textContent = set.title;
                     set_description_div.textContent = set.description;
                     
-                    overlay_left_img.src = "images/" + board.holds[set.left].image;
-                    overlay_right_img.src = "images/" + board.holds[set.right].image;
+                    document.querySelector("#run_content .overlay_left").src = "images/" + board.holds[set.left].image;
+                    document.querySelector("#run_content .overlay_right").src = "images/" + board.holds[set.right].image;
                 }
                 if (set.pause - 5 <= step) {
-                    tick_sound.play();
+                    await ticSound();
                 }
             }
         );
@@ -195,12 +203,143 @@ async function runTraining(training) {
     }
 }
 
-var finish_sound = new Sound("images/bell.mp3");
-var tick_sound = new Sound("images/countdown.mp3");
+function fillTrainingSelect(board_id) {
+    var training_select = document.getElementsByName('training_select')[0];
+    while (training_select.firstChild) {
+        training_select.removeChild(training_select.firstChild);
+    }
+    var first = true;
+    for (var training_num in trainings) {
+        var training = trainings[training_num];
+        if (training.board == board_id) {
+            var opt = document.createElement('option');
+            opt.setAttribute('value', training_num);
+            if (first) {
+                opt.setAttribute('selected', 'selected');
+                showTrainingDetails(board_id, training_num);
+                first = false;
+            }
+            var content = document.createTextNode(training.title);
+            opt.appendChild(content);
+            training_select.appendChild(opt);
+        }
+    }
+}
+
+function getBoard(board_id) {
+    for (var board of boards) {
+        if (board.id == board_id) {
+            return board;
+        }
+    }
+}    
+
+function showTrainingDetails(board_id, training_num) {
+    var training_details = document.getElementById('training_details');
+    while (training_details.firstChild) {
+        training_details.removeChild(training_details.firstChild);
+    }
+    var training = trainings[training_num];
+
+    var h2 = document.createElement('h2');
+    h2.setAttribute('class', 'training_title');
+    var content = document.createTextNode(training.title);
+    h2.appendChild(content);
+    training_details.appendChild(h2);
+
+    var p = document.createElement('p');
+    p.setAttribute('class', 'training_description');
+    content = document.createTextNode(training.description);
+    p.appendChild(content);
+    training_details.appendChild(p);
+    
+    var board = getBoard(board_id);
+    
+    for (var set of training.sets) {
+        var div = document.createElement('div');
+        div.setAttribute('class', 'training_set');
+        training_details.appendChild(div);
+        
+        var h3 = document.createElement('h3');
+        h3.setAttribute('class', 'set_title');
+        var content = document.createTextNode(set.title);
+        h3.appendChild(content);
+        div.appendChild(h3);
+
+        var outer = document.createElement('div');
+        outer.setAttribute('class', 'board_small_container');
+        var img1 = document.createElement('img');
+        img1.setAttribute('class', 'board_img');
+        img1.setAttribute('src', "images/" + board.image);
+        img1.setAttribute('alt', "");
+        outer.appendChild(img1);
+        var img2 = document.createElement('img');
+        img2.setAttribute('class', 'overlay_img overlay_left');
+        img2.setAttribute('src', "images/" + board.holds[set.left].image);
+        img2.setAttribute('alt', "");
+        outer.appendChild(img2);
+        var img3 = document.createElement('img');
+        img3.setAttribute('class', 'overlay_img overlay_right');
+        img3.setAttribute('src', "images/" + board.holds[set.right].image);
+        img3.setAttribute('alt', "");
+        outer.appendChild(img3);
+        div.appendChild(outer);
+
+        var p = document.createElement('p');
+        p.setAttribute('class', 'set_description');
+        content = document.createTextNode(set.description);
+        p.appendChild(content);
+        div.appendChild(p);
+        
+        p = document.createElement('p');
+        p.setAttribute('class', 'set_details');
+        content = document.createTextNode('Hold for ' + set.hold + " seconds. Interrupt for " + set.break + " seconds. Repeat " + set.reps + " times.");
+        p.appendChild(content);
+        div.appendChild(p);
+    }
+}
+
+pause_button.addEventListener("click", counter.pause);
+
+var start_button = document.getElementsByName('start')[0];
+
+start_button.addEventListener("click", function() {
+    var selected_board = board_select.options[board_select.selectedIndex].value;
+    var selected_training = training_select.options[training_select.selectedIndex].value;
+    document.getElementById("selector_content").style.display = "none";
+    document.getElementById("run_content").style.display = "block";
+    runTraining(selected_board, trainings[selected_training]);
+});
 
 selectVoice();
 if (speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = selectVoice;
 }
 
-runTraining(training1);
+var board_select = document.getElementsByName('board_select')[0];
+var training_select = document.getElementsByName('training_select')[0];
+var first = true;
+for (var board of boards) {
+    var opt = document.createElement('option');
+    opt.setAttribute('value', board.id);
+    if (first) {
+        opt.setAttribute('selected', 'selected');
+        first = false;
+    }
+    var content = document.createTextNode(board.name);
+    opt.appendChild(content);
+    board_select.appendChild(opt);
+}
+board_select.onchange = function(event) {
+    var selected_board = board_select.options[board_select.selectedIndex].value;
+    console.log("selected: " + selected_board);
+    fillTrainingSelect(selected_board);
+}
+training_select.onchange = function(event) {
+    var selected_board = board_select.options[board_select.selectedIndex].value;
+    var selected_training = training_select.options[training_select.selectedIndex].value;
+    console.log("selected: " + selected_training);
+    showTrainingDetails(selected_board, selected_training);
+}
+fillTrainingSelect(board_select.options[board_select.selectedIndex].value);
+
