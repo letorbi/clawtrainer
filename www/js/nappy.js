a9173d77a276da5be946301827ebbdf0281d900e
schroeer <schroeer@9472904b-5c12-0410-92ad-c346ed384f73>
2018-01-21 20:42:55 +0000

    neuer Router

--- www/js/nappy.js
+++ www/js/nappy.js
@@ -73,6 +73,16 @@ function completedSound() {
     soundEffect(1174.66, 0, 0.3, "square", 1, 0, 0.2);
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications
+function downloadTrainings() {
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(TRAININGS, null, "  "));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", "trainings.json");
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+}
+
 async function runTraining(board, training) {
     var set_title_div = document.getElementById("set_title");
     var set_description_div = document.getElementById("set_description");
@@ -223,6 +233,105 @@ async function runTraining(board, training) {
     }
 }
 
+function updateMainPage() {
+    var board_select = document.getElementsByName('board_select')[0];
+    var training_select = document.getElementsByName('training_select')[0];
+    var selected_training = training_select.selectedIndex;
+    if (selected_training == -1)
+        selected_training = 0;
+    console.log(selected_training);
+    
+    for (var board_num in BOARDS) {
+        var opt = document.createElement('option');
+        opt.setAttribute('value', board_num);
+        var content = document.createTextNode(BOARDS[board_num].name);
+        opt.appendChild(content);
+        board_select.appendChild(opt);
+    }
+    board_select.selectedIndex = 0;
+    fillTrainingSelect(selected_training);
+    
+    board_select.onchange = fillTrainingSelect;    training_select.onchange = showTrainingDetails;
+
+    function fillTrainingSelect(selected = 0) {
+        var board_num = board_select.options[board_select.selectedIndex].value;
+        while (training_select.firstChild) {
+            training_select.removeChild(training_select.firstChild);
+        }
+        for (var training_num in TRAININGS) {
+            var training = TRAININGS[training_num];
+            if (training.board == BOARDS[board_num].id) {
+                var opt = document.createElement('option');
+                opt.setAttribute('value', training_num);
+                var content = document.createTextNode(training.title);
+                opt.appendChild(content);
+                training_select.appendChild(opt);
+            }
+        }
+        training_select.selectedIndex = selected;
+        showTrainingDetails();
+    }
+
+    function showTrainingDetails() {
+        var selected_board_num = board_select.options[board_select.selectedIndex].value;
+        var selected_training_num = training_select.options[training_select.selectedIndex].value;
+        var board = BOARDS[selected_board_num];
+        var training = TRAININGS[selected_training_num];
+
+        var training_details = document.getElementById('training_details');
+        while (training_details.firstChild) {
+            training_details.removeChild(training_details.firstChild);
+        }
+
+        addElement(training_details, 'h2', training.title, {'class': 'training_title'});
+        addElement(training_details, 'p', training.description.replace(/([^.])$/, '$1.'), {'class': 'training_description'});
+        var times = calculateTimes(training);
+        addElement(training_details, 'p', `Total time: ${Math.floor(times[3] / 60)}:${(times[3] % 60).toString().padStart(2, "0")} min. Hang time: ${Math.floor(times[0] / 60)}:${(times[0] % 60).toString().padStart(2, "0")} min.`, {'class': 'training_description'});
+        
+        for (var set_num in training.sets) {
+            var set = training.sets[set_num];
+            
+            var pause_div = addElement(training_details, 'div', `Pause for ${set.pause} seconds.`, {'class': 'training_pause'});
+            
+            var div = addElement(training_details, 'div', null, {'class': 'training_set'});
+            
+            addElement(div, 'h3', (Number(set_num) + 1) + ". " + set.title, {'class': 'set_title'});
+
+            var outer = addElement(div, 'div', null, {'class': 'board_small_container'});
+
+            addElement(outer, 'img', null, {'class': 'board_img', 'src': "images/" + board.image, 'alt': ""});
+            addElement(outer, 'img', null, {'class': 'overlay_img overlay_left', 'src': (board.left_holds[set.left].image ? "images/" + board.left_holds[set.left].image : ""), 'alt': ""});
+            addElement(outer, 'img', null, {'class': 'overlay_img overlay_right', 'src': (board.right_holds[set.right].image ? "images/" + board.right_holds[set.right].image : ""), 'alt': ""});
+
+            addElement(div, 'p', set.description.replace(/([^.])$/, '$1.'), {'class': 'set_description'});
+            addElement(div, 'p', `Hold for ${set.hold} seconds. Interrupt for ${set.break} seconds. Repeat ${set.reps} times.`, {'class': 'set_details'});
+        }
+        
+        function addElement(node, type, text, atts) {
+            var el = document.createElement(type);
+            if (text) {
+                var tn = document.createTextNode(text);
+                el.appendChild(tn);
+            }
+            for (var name in atts) {
+                el.setAttribute(name, atts[name]);
+            }
+            node.appendChild(el);
+            return el;
+        }
+        
+        function calculateTimes(training) {
+            var pause = 0, inter = 0, hold = 0;
+            for (var set of training.sets) {
+                pause += set.pause;
+                inter += (set.reps - 1) * set.break;
+                hold += set.reps * set.hold;
+            }
+            return [hold, inter, pause, hold + inter + pause];
+        }
+    }
+}
+
 function updateEditPage(training_num) {
     var board_num = 0;
     var training = TRAININGS[training_num];
@@ -271,7 +380,7 @@ function updateEditPage(training_num) {
     let button_done = fragment.querySelector('button[name=edit_complete]');
     button_done.addEventListener("click", async function editDone() {
         // save
-        showMenu();
+        navigateTo("");
     });
 
     edit_content.appendChild(fragment);
@@ -416,61 +525,90 @@ function updateEditPage(training_num) {
     }
 }
 
-function showMenu() {
-    document.getElementById("menu_content").style.display = "block";
-    document.getElementById("run_content").style.display = "none";
-    document.getElementById("edit_content").style.display = "none";
-    window.scrollTo(0,0);
-    window.location.hash = "";
+function navigateTo(page) {
+    window.location.hash = page;
 }
 
-function showRun() {
-    document.getElementById("menu_content").style.display = "none";
-    document.getElementById("run_content").style.display = "block";
-    document.getElementById("edit_content").style.display = "none";
+function handleRouting(event) {
+    var match = location.hash.match(/#?([^_]+)_?(\d*)/);
+    var new_page = match ? match[1] : "";
+    var new_num = match ? match[2] : null;
+    var old_page = "", old_num = null;
+    if (event) {
+        match = event.oldURL.match(/[^#]*#?([^_]+)_?(\d*)/);
+        old_page = match ? match[1] : "";
+        old_num = match ? match[2] : null;
+    }
+    console.log("Navigating to " + new_page);
+    if (old_page == "run") {
+        // TODO: are you sure?
+        COUNTER.stop();
+    }
     window.scrollTo(0,0);
-    window.location.hash = "run";
-}
-
-function showEdit() {
-    document.getElementById("menu_content").style.display = "none";
-    document.getElementById("run_content").style.display = "none";
-    document.getElementById("edit_content").style.display = "block";
-    window.scrollTo(0,0);
-    window.location.hash = "edit";
+    switch (new_page) {
+        case "":
+            updateMainPage();
+            document.getElementById("toolbar_title").innerText = "nappy fingers";
+            document.getElementById("toolbar_icon_back").style.display = "none";
+            document.getElementById("toolbar_icon_menu").style.display = "inline";
+            document.getElementById("main_content").style.display = "block";
+            document.getElementById("run_content").style.display = "none";
+            document.getElementById("edit_content").style.display = "none";
+            break;
+        case "edit":
+            updateEditPage(new_num);
+            document.getElementById("toolbar_title").innerText = "Editor";
+            document.getElementById("toolbar_icon_back").style.display = "inline";
+            document.getElementById("toolbar_icon_menu").style.display = "none";
+            document.getElementById("main_content").style.display = "none";
+            document.getElementById("run_content").style.display = "none";
+            document.getElementById("edit_content").style.display = "block";
+            break;
+        case "run":
+            document.getElementById("toolbar_title").innerText = "Trainer";
+            document.getElementById("toolbar_icon_back").style.display = "inline";
+            document.getElementById("toolbar_icon_menu").style.display = "none";
+            document.getElementById("main_content").style.display = "none";
+            document.getElementById("run_content").style.display = "block";
+            document.getElementById("edit_content").style.display = "none";
+            break;
+    }
 }
 
 function init() {
     var board_select = document.getElementsByName('board_select')[0];
     var training_select = document.getElementsByName('training_select')[0];
     
-    window.onhashchange = function hashChanged(event) {
-        console.log("Hash changed to " + window.location.hash);
-    };
+    window.addEventListener('hashchange', handleRouting);
+
+    window.addEventListener("backbutton", function popstate(event) {
+        console.log("backbutton event " + event);
+    }, false);
+
+    StatusBar.hide();
 
     var start_button = document.getElementsByName('start')[0];
     start_button.addEventListener("click", async function startTraining() {
-        showRun();
         var selected_board_num = board_select.options[board_select.selectedIndex].value;
         var selected_training_num = training_select.options[training_select.selectedIndex].value;
+        navigateTo("run_" + selected_training_num);
         try {
             window.plugins.insomnia.keepAwake();
             await runTraining(BOARDS[selected_board_num], TRAININGS[selected_training_num]);
+            navigateTo("");
         }
         catch (err) {
             console.log(`Training aborted (${err})`);
         }
         finally {
             window.plugins.insomnia.allowSleepAgain();
-            showMenu();
         }
     });
 
     var edit_button = document.getElementsByName('edit')[0];
     edit_button.addEventListener("click", async function editTraining() {
         var selected_training_num = training_select.options[training_select.selectedIndex].value;
-        updateEditPage(selected_training_num);
-        showEdit();
+        navigateTo(`edit_${selected_training_num}`);
     });
 
     var clone_button = document.getElementsByName('clone')[0];
@@ -478,9 +616,7 @@ function init() {
         var selected_training_num = Number(training_select.options[training_select.selectedIndex].value);
         TRAININGS.splice(selected_training_num, 0, JSON.parse(JSON.stringify(TRAININGS[selected_training_num])));
         TRAININGS[selected_training_num + 1].title += " (copy)";
-        fillTrainingSelect(selected_training_num + 1);
-        updateEditPage(selected_training_num + 1);
-        showEdit();
+        navigateTo(`edit_${selected_training_num + 1}`);
     });
 
     var delete_button = document.getElementsByName('delete')[0];
@@ -489,114 +625,17 @@ function init() {
         // TODO: confirm
         TRAININGS.splice(selected_training_num, 1);
         // TODO: save
-        fillTrainingSelect(0);
+        updateMainPage();
     });
 
     var pause_button = document.getElementsByName("pause")[0];
     pause_button.addEventListener("click", COUNTER.pause);
 
     var stop_button = document.getElementsByName("stop")[0];
-    stop_button.addEventListener("click", COUNTER.stop);
+    stop_button.addEventListener("click", function stopRun() {
+        navigateTo("");
+    });
 
-    for (var board_num in BOARDS) {
-        var opt = document.createElement('option');
-        opt.setAttribute('value', board_num);
-        var content = document.createTextNode(BOARDS[board_num].name);
-        opt.appendChild(content);
-        board_select.appendChild(opt);
-    }
-    board_select.selectedIndex = 0;
-    fillTrainingSelect();
-    
-    board_select.onchange = fillTrainingSelect;
-    training_select.onchange = showTrainingDetails;
-
-    function fillTrainingSelect(selected = 0) {
-        var board_num = board_select.options[board_select.selectedIndex].value;
-        while (training_select.firstChild) {
-            training_select.removeChild(training_select.firstChild);
-        }
-        for (var training_num in TRAININGS) {
-            var training = TRAININGS[training_num];
-            if (training.board == BOARDS[board_num].id) {
-                var opt = document.createElement('option');
-                opt.setAttribute('value', training_num);
-                var content = document.createTextNode(training.title);
-                opt.appendChild(content);
-                training_select.appendChild(opt);
-            }
-        }
-        training_select.selectedIndex = selected;
-        showTrainingDetails();
-    }
-
-    function showTrainingDetails() {
-        var selected_board_num = board_select.options[board_select.selectedIndex].value;
-        var selected_training_num = training_select.options[training_select.selectedIndex].value;
-        var board = BOARDS[selected_board_num];
-        var training = TRAININGS[selected_training_num];
-
-        var training_details = document.getElementById('training_details');
-        while (training_details.firstChild) {
-            training_details.removeChild(training_details.firstChild);
-        }
-
-        addElement(training_details, 'h2', training.title, {'class': 'training_title'});
-        addElement(training_details, 'p', training.description.replace(/([^.])$/, '$1.'), {'class': 'training_description'});
-        var times = calculateTimes(training);
-        addElement(training_details, 'p', `Total time: ${Math.floor(times[3] / 60)}:${(times[3] % 60).toString().padStart(2, "0")} min. Hang time: ${Math.floor(times[0] / 60)}:${(times[0] % 60).toString().padStart(2, "0")} min.`, {'class': 'training_description'});
-        
-        for (var set_num in training.sets) {
-            var set = training.sets[set_num];
-            
-            var pause_div = addElement(training_details, 'div', `Pause for ${set.pause} seconds.`, {'class': 'training_pause'});
-            
-            var div = addElement(training_details, 'div', null, {'class': 'training_set'});
-            
-            addElement(div, 'h3', (Number(set_num) + 1) + ". " + set.title, {'class': 'set_title'});
-
-            var outer = addElement(div, 'div', null, {'class': 'board_small_container'});
-
-            addElement(outer, 'img', null, {'class': 'board_img', 'src': "images/" + board.image, 'alt': ""});
-            addElement(outer, 'img', null, {'class': 'overlay_img overlay_left', 'src': (board.left_holds[set.left].image ? "images/" + board.left_holds[set.left].image : ""), 'alt': ""});
-            addElement(outer, 'img', null, {'class': 'overlay_img overlay_right', 'src': (board.right_holds[set.right].image ? "images/" + board.right_holds[set.right].image : ""), 'alt': ""});
-
-            addElement(div, 'p', set.description.replace(/([^.])$/, '$1.'), {'class': 'set_description'});
-            addElement(div, 'p', `Hold for ${set.hold} seconds. Interrupt for ${set.break} seconds. Repeat ${set.reps} times.`, {'class': 'set_details'});
-        }
-        
-        function addElement(node, type, text, atts) {
-            var el = document.createElement(type);
-            if (text) {
-                var tn = document.createTextNode(text);
-                el.appendChild(tn);
-            }
-            for (var name in atts) {
-                el.setAttribute(name, atts[name]);
-            }
-            node.appendChild(el);
-            return el;
-        }
-        
-        function calculateTimes(training) {
-            var pause = 0, inter = 0, hold = 0;
-            for (var set of training.sets) {
-                pause += set.pause;
-                inter += (set.reps - 1) * set.break;
-                hold += set.reps * set.hold;
-            }
-            return [hold, inter, pause, hold + inter + pause];
-        }
-    }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications
-function downloadTrainings() {
-    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(TRAININGS, null, "  "));
-    var downloadAnchorNode = document.createElement('a');
-    downloadAnchorNode.setAttribute("href", dataStr);
-    downloadAnchorNode.setAttribute("download", "trainings.json");
-    downloadAnchorNode.click();
-    downloadAnchorNode.remove();
+    handleRouting();
 }
 
